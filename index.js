66b05dcc213159cb50354edea776196746bb594a
EricDosReis <ericdosreis.personal@gmail.com>
2025-09-11 14:47:10 -0300

    feat: get table name and columns from CREATE TABLE statement

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Go');
+const statement = 'CREATE TABLE author (id number, name string, age number, city string, state string, country string)';
+
+const createTableRegExp = /CREATE TABLE ([a-z]+) \((.+)\)/;
+const [, tableName, rawColumns] = statement.match(createTableRegExp);
+const columns = rawColumns.split(',').map(column => column.trim());
+
+console.log(tableName);
+console.log(columns);
