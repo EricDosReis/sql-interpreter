c1a949a69aef909209e8683ac740523caaaf88d9
EricDosReis <ericdosreis.personal@gmail.com>
2025-09-18 12:00:01 -0300

    refactor: implement createTable and execute methods to make database more robust

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
+function Database() {
+  this.tables = {};
+}
+
+Database.prototype.createTable = function(statement) {
+  const createTableRegExp = /CREATE TABLE ([a-z]+) \((.+)\)/;
+  const [, tableName, rawColumns] = statement.match(createTableRegExp);
+  const columns = rawColumns.split(',').map(column => column.trim());
+
+  this.tables[tableName] = {
+    columns: {},
+    data: [],
+  };
+
+  columns.forEach((item) => {
+    const [name, type] = item.split(' ');
+
+    this.tables[tableName].columns[name] = type;
+  })
+}
+
+Database.prototype.execute = function(statement) {
+  if (statement.startsWith('CREATE TABLE')) {
+    this.createTable(statement);
+  } else {
+    console.error('Unknown statement');
+  }
+}
+
+const database = new Database();
 const statement = 'CREATE TABLE author (id number, name string, age number, city string, state string, country string)';
 
-const createTableRegExp = /CREATE TABLE ([a-z]+) \((.+)\)/;
-const [, tableName, rawColumns] = statement.match(createTableRegExp);
-const columns = rawColumns.split(',').map(column => column.trim());
-
-const database = {
-  tables: {
-    [tableName]: {
-      columns: {},
-      data: [],
-    },
-  },
-};
-
-columns.forEach((item) => {
-  const [name, type] = item.split(' ');
-
-  database.tables[tableName].columns[name] = type;
-})
+database.execute(statement)
 
 console.log(JSON.stringify(database, null, 2));
