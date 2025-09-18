9b2dbffda335aa50ad4e624babb73515c91101b2
EricDosReis <ericdosreis.personal@gmail.com>
2025-09-18 17:14:00 -0300

    feat: throw error for unknown commands

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function DatabaseError(statement, message) {
+  this.message = message;
+  this.statement = statement;
+}
+
 function Database() {
   this.tables = {};
 }
@@ -23,13 +28,19 @@ Database.prototype.execute = function(statement) {
   if (statement.startsWith('CREATE TABLE')) {
     this.createTable(statement);
   } else {
-    console.error('Unknown statement');
+    throw new DatabaseError(statement, `Syntax error: "${statement}"`);
   }
 }
 
 const database = new Database();
-const statement = 'CREATE TABLE author (id number, name string, age number, city string, state string, country string)';
+const createTableStatement = 'CREATE TABLE author (id number, name string, age number, city string, state string, country string)';
+const selectAuthorStatement = 'SELECT id, name FROM author';
 
-database.execute(statement)
+try {
+  database.execute(createTableStatement);
+  console.log(JSON.stringify(database, null, 2));
 
-console.log(JSON.stringify(database, null, 2));
+  database.execute(selectAuthorStatement);
+} catch (error) {
+  console.log(error.message);
+}
