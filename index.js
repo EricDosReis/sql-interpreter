e4caf77a41662f8c1ed897f8047c4bd27e82e19e
EricDosReis <ericdosreis.personal@gmail.com>
2025-09-23 16:28:49 -0300

    refactor: convert constructor functions to classes

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function Parser() {
-  const commands = new Map([
-    ['createTable', /CREATE TABLE ([a-z]+) \((.+)\)/],
-    ['insert', /INSERT INTO ([a-z]+) \((.+)\) VALUES \((.+)\)/],
-    ['delete', /DELETE FROM ([a-z]+)(?: WHERE (.+))?/],
-    ['select', /SELECT (.+) FROM ([a-z]+)(?: WHERE (.+))?/]
-  ]);
+class Parser {
+  constructor() {
+    this.commands = new Map([
+      ['createTable', /CREATE TABLE ([a-z]+) \((.+)\)/],
+      ['insert', /INSERT INTO ([a-z]+) \((.+)\) VALUES \((.+)\)/],
+      ['delete', /DELETE FROM ([a-z]+)(?: WHERE (.+))?/],
+      ['select', /SELECT (.+) FROM ([a-z]+)(?: WHERE (.+))?/]
+    ]);
+  }
 
-  this.parse = function(statement) {
-    for (let [command, regExp] of commands) {
+  parse(statement) {
+    for (let [command, regExp] of this.commands) {
       const parsedStatement = statement.match(regExp);
 
       if (parsedStatement) {
@@ -20,101 +22,104 @@ function Parser() {
   }
 }
 
-function DatabaseError(statement, message) {
-  this.message = message;
-  this.statement = statement;
-}
-
-function Database() {
-  this.tables = {};
-  this.parser = new Parser();
-}
-
-Database.prototype.createTable = function(parsedStatement) {
-  const [, tableName, rawColumns] = parsedStatement;
-  const columns = rawColumns.split(',').map(column => column.trim());
-
-  this.tables[tableName] = {
-    columns: {},
-    data: [],
-  };
-
-  columns.forEach((item) => {
-    const [name, type] = item.split(' ');
-
-    this.tables[tableName].columns[name] = type;
-  })
-}
-
-Database.prototype.insert = function(parsedStatement) {
-  const [, tableName, rawColumns, rawValues] = parsedStatement;
-
-  const columns = rawColumns.split(',').map(column => column.trim());
-  const values = rawValues.split(',').map(value => value.trim());
-
-  const row = columns.reduce((acc, column, index) => {
-    acc[column] = values[index];
-
-    return acc;
-  }, {});
-
-  this.tables[tableName].data.push(row);
-}
-
-Database.prototype.delete = function(parsedStatement) {
-  const [, tableName, whereClause] = parsedStatement;
-
-  if (whereClause) {
-    const [whereColumn, whereValue] = whereClause.split('=').map(value => value.trim());
-
-    this.tables[tableName].data = this.tables[tableName].data.filter(row => row[whereColumn] !== whereValue);
-  } else {
-    this.tables[tableName].data = [];
+class DatabaseError {
+  constructor(statement, message) {
+    this.message = message;
+    this.statement = statement;
   }
 }
 
-Database.prototype.select = function(parsedStatement) {
-  const [, rawSelectColumns, tableName, whereClause] = parsedStatement;
-  const selectColumns = rawSelectColumns.split(',').map(column => column.trim());
-  let rows = [];
-
-  if (whereClause) {
-    const [whereColumn, whereValue] = whereClause.split('=').map(value => value.trim());
-
-    rows = this.tables[tableName].data.filter(row => row[whereColumn] === whereValue);
-  } else {
-    rows = this.tables[tableName].data;
+class Database {
+  constructor() {
+    this.tables = {};
+    this.parser = new Parser();
   }
 
-  if (selectColumns.includes('*') === false && rows.length > 0) {
-    rows = rows
-      .map((row) => {
-        return selectColumns.reduce((acc, column) => {
-          acc[column] = row[column];
+  createTable(parsedStatement) {
+    const [, tableName, rawColumns] = parsedStatement;
+    const columns = rawColumns.split(',').map(column => column.trim());
 
-          return acc;
-        }, {});
-      });
+    this.tables[tableName] = {
+      columns: {},
+      data: [],
+    };
+
+    columns.forEach((item) => {
+      const [name, type] = item.split(' ');
+
+      this.tables[tableName].columns[name] = type;
+    });
   }
 
-  return rows;
-}
+  insert(parsedStatement) {
+    const [, tableName, rawColumns, rawValues] = parsedStatement;
 
-Database.prototype.execute = function(statement) {
-  const result = this.parser.parse(statement);
+    const columns = rawColumns.split(',').map(column => column.trim());
+    const values = rawValues.split(',').map(value => value.trim());
 
-  if (result) {
-    return this[result.command](result.parsedStatement);
+    const row = columns.reduce((acc, column, index) => {
+      acc[column] = values[index];
+
+      return acc;
+    }, {});
+
+    this.tables[tableName].data.push(row);
   }
 
-  throw new DatabaseError(statement, `Unknown statement: '${statement}'`);
+  delete(parsedStatement) {
+    const [, tableName, whereClause] = parsedStatement;
+
+    if (whereClause) {
+      const [whereColumn, whereValue] = whereClause.split('=').map(value => value.trim());
+
+      this.tables[tableName].data = this.tables[tableName].data.filter(row => row[whereColumn] !== whereValue);
+    } else {
+      this.tables[tableName].data = [];
+    }
+  }
+
+  select(parsedStatement) {
+    const [, rawSelectColumns, tableName, whereClause] = parsedStatement;
+    const selectColumns = rawSelectColumns.split(',').map(column => column.trim());
+    let rows = [];
+
+    if (whereClause) {
+      const [whereColumn, whereValue] = whereClause.split('=').map(value => value.trim());
+
+      rows = this.tables[tableName].data.filter(row => row[whereColumn] === whereValue);
+    } else {
+      rows = this.tables[tableName].data;
+    }
+
+    if (selectColumns.includes('*') === false && rows.length > 0) {
+      rows = rows
+        .map((row) => {
+          return selectColumns.reduce((acc, column) => {
+            acc[column] = row[column];
+
+            return acc;
+          }, {});
+        });
+    }
+
+    return rows;
+  }
+
+  execute(statement) {
+    const result = this.parser.parse(statement);
+
+    if (result) {
+      return this[result.command](result.parsedStatement);
+    }
+
+    throw new DatabaseError(statement, `Unknown statement: '${statement}'`);
+  }
 }
 
 const database = new Database();
-const createTableStatement = 'CREATE TABLE author (id number, name string, age number, city string, state string, country string)';
 
 try {
-  database.execute(createTableStatement);
+  database.execute('CREATE TABLE author (id number, name string, age number, city string, state string, country string)');
   database.execute('INSERT INTO author (id, name, age) VALUES (1, Douglas Crockford, 62)');
   database.execute('INSERT INTO author (id, name, age) VALUES (2, Linus Torvalds, 47)');
   database.execute('INSERT INTO author (id, name, age) VALUES (3, Martin Fowler, 54)');
